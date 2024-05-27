aeebd07ff39b40b1a741f5124cb438c53a998676
Louis Bailleau <louis13.bailleau@gmail.com>
2024-05-27 21:53:57 +0200

    ci: add downloader image build workflow

--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -86,7 +86,7 @@ func main() {
 		c.NextProtos = []string{"http/1.1"}
 	}
 
-	tlsOpts := []func(*tls.Config){}
+	var tlsOpts []func(*tls.Config)
 	if !enableHTTP2 {
 		tlsOpts = append(tlsOpts, disableHTTP2)
 	}
