8b95c64df5242a02ed63772ada261b939551c0fd
Louis Bailleau <louis13.bailleau@gmail.com>
2025-01-26 18:14:16 +0100

    feat: convert logger into a json logger

--- projects/services/kubestro-core/backend/src/main.rs
+++ projects/services/kubestro-core/backend/src/main.rs
@@ -2,15 +2,19 @@ use std::time::Duration;
 
 use axum::{
     extract::{MatchedPath, Request},
-    http::{request, StatusCode},
+    http::StatusCode,
     response::IntoResponse,
     routing::get,
     Json, Router,
 };
 use serde::Serialize;
 use tokio::{net::TcpListener, signal};
-use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
+use tower_http::{
+    timeout::TimeoutLayer,
+    trace::{self, TraceLayer},
+};
 use tracing::info_span;
+use tracing::Level;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -26,34 +30,37 @@ async fn main() {
                 format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
             }),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().with_target(false).json())
         .init();
 
     // build our application with a route
-    let app = Router::new().route("/", get(handler)).layer((
-        TraceLayer::new_for_http()
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer((
+            // TraceLayer::new_for_http()
             // Create our own span for the request and include the matched path. The matched
             // path is useful for figuring out which handler the request was routed to.
-            .make_span_with(|req: &Request| {
-                let method = req.method();
-                let uri = req.uri();
+            TraceLayer::new_for_http()
+                // Create our own span for the request and include the matched path. The matched
+                // path is useful for figuring out which handler the request was routed to.
+                .make_span_with(|req: &Request| {
+                    let method = req.method();
+                    let uri = req.uri();
 
-                // axum automatically adds this extension
-                let matched_path = req
-                    .extensions()
-                    .get::<MatchedPath>()
-                    .map(|matched_path| matched_path.as_str());
+                    // axum automatically adds this extension
+                    let matched_path = req
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(|matched_path| matched_path.as_str());
 
-                tracing::debug_span!("request", %method, %uri, matched_path)
-            })
-            // By default `TraceLayer` will log 5xx responses but we're doing our specific
+                    tracing::debug_span!("request", %method, %uri, matched_path)
+                })
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)), // By default `TraceLayer` will log 5xx responses but we're doing our specific
             // logging of errors so disable that
-            .on_failure(()),
-        TimeoutLayer::new(Duration::from_secs(10)),
-    ));
-
-    // add a fallback service for handling routes to unknown paths
-    let app = app.fallback(handler_404);
+            TimeoutLayer::new(Duration::from_secs(10)),
+        ))
+        // add a fallback service for handling routes to unknown paths
+        .fallback(handler_404);
 
     // run it
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
