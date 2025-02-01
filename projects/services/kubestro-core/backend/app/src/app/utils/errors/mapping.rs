81d34d1ee0626c425b864e4e0ac440106ed8efd1
Louis Bailleau <louis13.bailleau@gmail.com>
2025-02-01 21:18:38 +0100

    feat: add helpers to `ApiError` for generic use-cases

--- projects/services/kubestro-core/backend/app/src/app/utils/errors/mapping.rs
+++ projects/services/kubestro-core/backend/app/src/app/utils/errors/mapping.rs
@@ -1,9 +1,33 @@
 use axum::http::StatusCode;
 use serde::{Serialize, Serializer};
 
+use super::ApiError;
+
 pub fn map_status_code<S: Serializer>(
     status: &StatusCode,
     serializer: S,
 ) -> Result<S::Ok, S::Error> {
     status.as_u16().serialize(serializer)
+}
+
+impl ApiError {
+    pub fn unexpected_error(detail: impl ToString) -> Self {
+        Self {
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+            title: "Internal Server Error".into(),
+            detail: Some(format!("Unexpected error: {}", detail.to_string()).into()),
+            code: "internal_server_error".into(),
+            ..Default::default()
+        }
+    }
+
+    pub fn database_error(detail: impl ToString) -> Self {
+        Self {
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+            title: "Internal Server Error".into(),
+            detail: Some(format!("Database error: {}", detail.to_string()).into()),
+            code: "internal_server_error".into(),
+            ..Default::default()
+        }
+    }
 }
