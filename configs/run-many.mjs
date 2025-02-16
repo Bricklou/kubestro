7b3562a4369c70b99c58e1af34ef6bfce9bd9eda
Louis Bailleau <louis13.bailleau@gmail.com>
2025-02-16 16:23:24 +0100

    fix: remove typos and update run-many.mjs script shell calls

--- configs/run-many.mjs
+++ configs/run-many.mjs
@@ -7,18 +7,21 @@ const isMain = process.argv[5] === 'refs/heads/main'
 const baseSha = isMain ? 'origin/master~1' : 'origin/main'
 
 const affected = execSync(
-  `pnpm exec nx show projects --affected --base="${baseSha}" --target="${target}"`
+  `pnpm exec nx show projects --affected -t "${target}" --base="${baseSha}" --json`
 ).toString('utf8')
 
 const array = JSON.parse(affected)
-  .tasks.map(t => t.target.project)
   .slice()
   .sort()
 
+console.log('Affected projects:', array)
+
 const slideSize = Math.max(Math.floor(array.length / jobCount), 1)
 const projects = jobIndex < jobCount ?
-    array.slide(slideSize * (jobIndex - 1), slideSize * jobIndex) :
+    array.slice(slideSize * (jobIndex - 1), slideSize * jobIndex) :
     array.slice(slideSize * (jobIndex - 1))
+
+console.log('Projects to run:', projects)
 
 if (projects.length > 0) {
   execSync(
