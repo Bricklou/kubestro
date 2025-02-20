bf54ddbf5fd62bfc1efc40e3671135c67f319ef2
Louis Bailleau <louis13.bailleau@gmail.com>
2025-02-20 13:39:41 +0100

    feat: reworked the auth middleware

--- projects/services/kubestro-core/backend/app/src/app/http/routes/authentication/mod.rs
+++ projects/services/kubestro-core/backend/app/src/app/http/routes/authentication/mod.rs
@@ -1,5 +1,11 @@
+use axum::middleware;
 use utoipa::OpenApi;
-use utoipa_axum::{router::OpenApiRouter, routes};
+use utoipa_axum::{
+    router::{OpenApiRouter, UtoipaMethodRouterExt},
+    routes,
+};
+
+use crate::app::http::middlewares;
 
 mod login;
 mod logout;
@@ -21,7 +27,10 @@ pub fn get_routes() -> OpenApiRouter {
     OpenApiRouter::with_openapi(ApiDoc::openapi())
         .routes(routes!(login::handler_login))
         .routes(routes!(register::handler_register))
-        .routes(routes!(me::handler_me, logout::handler_logout))
+        .routes(
+            routes!(me::handler_me, logout::handler_logout)
+                .layer(middleware::from_fn(middlewares::auth::auth_middleware)),
+        )
         .routes(routes!(oidc::handler_oidc_redirect))
         .routes(routes!(oidc::handler_oidc_callback))
 }
